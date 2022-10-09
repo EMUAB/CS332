a2eb351410b5c31d05739b88048aa35baf822035
EMUAB <theservercrashed@gmail.com>
2022-10-09 13:19:35 -0500

    lab7 changes

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -5,32 +5,46 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+    char *line = NULL;
+    size_t maxlen = 0;
+    ssize_t n;
+
+    FILE *file = fopen("input.txt", O_RDONLY);
+    if (file == NULL) {
+        printf("Error opening input file");
+        exit(-1);
+    }
+
+    while ((n = getline(&line, &maxlen, file)) > 0) {
+        printf("%s\n", strtok(line, " "));
+    }
+    free(line);
+
     pid_t pid;
     int status;
-
     pid = fork();
     if (pid == 0) { /* this is child process */
-        printf(
-            "This is the child process, my PID is %ld and my parent PID is %ld\n",
-            (long)getpid(), (long)getppid());
-    } else if (pid > 0) { /* this is the parent process */
-        printf(
-            "This is the parent process, my PID is %ld and the child PID is %ld\n",
-            (long)getpid(), (long)pid);
+        printf("This is the child process, my PID is %ld and my parent PID is %ld\n", (long)getpid(), (long)getppid());
+    }
+    else if (pid > 0) { /* this is the parent process */
+        printf("This is the parent process, my PID is %ld and the child PID is %ld\n", (long)getpid(), (long)pid);
 
         printf("Wait for the child process to terminate\n");
-        wait(&status);           /* wait for the child process to terminate */
+        wait(&status);
+
         if (WIFEXITED(status)) { /* child process terminated normally */
             printf("Child process exited with status = %d\n",
                    WEXITSTATUS(status));
-        } else { /* child process did not terminate normally */
-            printf("ERROR: Child process did not terminate normally!\n");
-            /* look at the man page for wait (man 2 wait) to
-               determine how the child process was terminated */
         }
-    } else { /* we have an error in process creation */
+        else { /* child process did not terminate normally */
+            printf("ERROR: Child process did not terminate normally!\n");
+        }
+    }
+    else { /* we have an error in process creation */
         perror("fork");
         exit(EXIT_FAILURE);
     }
